317b5dd91ce94447476dc50794dd0d41fb5e9885
LeeKM321 <stephen4951@gmail.com>
2024-12-02 15:24:24 +0900

    orderservice-msa Jenkinsfile 수정 - 21

--- config-service/setup-ssh.sh
+++ config-service/setup-ssh.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# SSH private key를 환경 변수에서 읽어 파일로 저장
-echo "$GIT_PRIVATE_KEY" > /root/.ssh/id_ecdsa
+# .env 파일에서 PRIVATE KEY를 읽어옵니다.
+PRIVATE_KEY=${GIT_PRIVATE_KEY}
+
+# 줄바꿈(\n)을 개행 문자로 변경
+FORMATTED_KEY=$(echo -e "$PRIVATE_KEY")
+
+# SSH 키 파일 생성
+mkdir -p /root/.ssh
+cat <<EOF > /root/.ssh/id_ecdsa
+$FORMATTED_KEY
+EOF
 chmod 600 /root/.ssh/id_ecdsa
 
 # GitHub의 호스트 키를 known_hosts에 추가
