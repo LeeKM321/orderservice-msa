39774e89612381e9d59ca480a5eae81eb495f1f2
LeeKM321 <stephen4951@gmail.com>
2024-12-03 10:20:51 +0900

    orderservice-msa Jenkinsfile 수정 - 23

--- config-service/setup-ssh.sh
+++ config-service/setup-ssh.sh
@@ -4,9 +4,13 @@
 PRIVATE_KEY=${GIT_PRIVATE_KEY}
 
 # 줄바꿈(\n)을 개행 문자로 변경
+# echo -e -> 문자열에 작성된 \n, \t같은 탈출 코드를 실제로 반영해 줌.
+# .env에는 줄개행을 할 수 없었기 때문에 \n을 썼는데, 원본 키 파일은 줄개행이 반영이 되어야 하기 때문에
+# 변환 과정을 거친다.
 FORMATTED_KEY=$(echo -e "$PRIVATE_KEY")
 
 # SSH 키 파일 생성
+# <<EOF ~~~ EOF: 여러 줄의 텍스트를 파일에 작성할 수 있는 방법.
 mkdir -p /root/.ssh
 cat <<EOF > /root/.ssh/id_ecdsa
 $FORMATTED_KEY
